a73bdd42e8b3d9885cefc7dd90866d20b27af6b2
Joe Hallman <gohawkeyes74@yahoo.com>
2016-09-28 19:46:07 +0000

    more work on part 2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,31 @@
 class UsersController < ApplicationController
+  
+  def user_params
+    params.require(:user).permit(:user_id, :email, :session_token)
+  end
+  
   def create
+    @user = User.new(user_params)
+    
+    User.all.each do |i|
+      if i == @user.email
+        flash[:notice] = "Sorry, this user id is taken. Try again."
+        redirect_to movies_path
+      end
+    end
+    
+    emailRegex = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    if emailRegex =~ @user.email
+      flash[:notice] = "#{@user.user_id} was successfully created."
+      @user = User.create!(user_params)
+      redirect_to movies_path
+    else
+      flash[:notice] = "Invalid email."
+      redirect_to movies_path
+    end
+    
+    #flash[:notice] = "#{@user.user_id} was successfully created."
+    #redirect_to movies_path
   end
 
   def destroy
