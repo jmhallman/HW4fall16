d81d36586f7ec29dc8a7dcc8a2085aadc4b441be
Joe Hallman <gohawkeyes74@yahoo.com>
2016-09-28 04:18:24 +0000

    added stuff

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,46 @@
+class UsersController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  
+  def user_params
+    params.require(:user).permit(:user_id, :email, :session_token)
+  end
+
+  def show
+    id = params[:id] # retrieve movie ID from URI route
+    @user = User.find(id) # look up movie by unique ID
+    # will render app/views/movies/show.<extension> by default
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    # default: render 'new' template
+  end
+
+  def create
+    @user = User.create!(user_params)
+    flash[:notice] = "#{@user.user_id} was successfully created."
+    redirect_to musers_path
+  end
+
+  def edit
+    @user = User.find params[:id]
+  end
+
+  def update
+    @user = User.find params[:id]
+    @user.update_attributes!(user_params)
+    flash[:notice] = "#{@user.user_id} was successfully updated."
+    redirect_to user_path(@user)
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:notice] = "User '#{@user.user_id}' deleted."
+    redirect_to users_path
+  end
+end
